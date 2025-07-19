73297e21f36402777b506fa0831bbbb8bdab1c9f
venh <conf312@naver.com>
2025-07-19 19:18:19 +0900

    [+] 상품 가격 조건 삭제

--- service-batch/src/main/java/com/issuemoa/batch/infrastructure/writer/ProductPriceWriter.java
+++ service-batch/src/main/java/com/issuemoa/batch/infrastructure/writer/ProductPriceWriter.java
@@ -14,16 +14,15 @@ import java.util.List;
 @Slf4j
 @Component
 public class ProductPriceWriter implements ItemWriter<List<ProductPrice>> {
-
     private final ProductPriceRepository productPriceRepository;
 
     @Override
     public void write(List<? extends List<ProductPrice>> items) {
         log.info("==> [ProductPriceWriter] items.size :: {}", items.size());
-        List<ProductPrice> products = new ArrayList<>();
-
-        items.forEach(list -> list.forEach(products::add));
-
-        productPriceRepository.saveAll(products);
+        if (!items.isEmpty()) {
+            List<ProductPrice> products = new ArrayList<>();
+            items.forEach(list -> list.forEach(products::add));
+            productPriceRepository.saveAll(products);
+        }
     }
 }
